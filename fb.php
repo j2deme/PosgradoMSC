d8bb521ab8697d2148ed37d2bf14a6495856106c
Jaime Jesús Delgado Meraz <j2deme@gmail.com>
2012-05-09 13:16:47 -0500

    FB Oauth

--- fb.php
+++ fb.php
@@ -1,5 +1,5 @@
 <?php
-require '../vendor/facebook-sdk/facebook.php';
+require './vendor/facebook-sdk/facebook.php';
 
 // Create our Application instance (replace this with your appId and secret).
 $facebook = new Facebook(array(
