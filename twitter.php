557fdd8c3a9b9e416616a977d378656d2bf82a54
Jaime Jesús Delgado Meraz <j2deme@gmail.com>
2012-05-10 14:56:14 -0500

    Changed twitter class

--- twitter.php
+++ twitter.php
@@ -6,7 +6,7 @@ require_once './vendor/twitter/twitter.class.php';
 $consumerKey 	= "bhqtdonKSjPec3lObO7h4w";
 $consumerSecret = "bB2ohDnvAQJZExhbics52LTpe1vuf0mAeHR4dqizo";
 $accessToken	= "13940702-T0sQk1WKxeb6gJB7rqDEnnR2tdNbwgEWcey5Rwpyk";
-$accesTokenSecret = "YFac9K1zDeVMKD93VhDaRM0C6J4BrHdqAyTl8qYNis";
+$accessTokenSecret = "YFac9K1zDeVMKD93VhDaRM0C6J4BrHdqAyTl8qYNis";
 
 $twitter = new Twitter($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
 $status = $twitter->send('Hola este es un tweet de prueba');
