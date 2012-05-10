1dc16f9a1e7b235c5bd6eb961da9413ac4a306fd
Jaime Jesús Delgado Meraz <j2deme@gmail.com>
2012-05-10 14:47:28 -0500

    Changed twitter class

--- send.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once 'twitter.class.php';
-
-$twitter = new Twitter('pokusnyucet2', '123456');
-$status = $twitter->send('I am fine');
-
-echo $status ? 'OK' : 'ERROR';
--- twitter.php
+++ twitter.php
@@ -1,10 +1,15 @@
 <?php
 ini_set ('error_reporting', E_ALL);
 
-require_once './vendor/twitter.class.php';
+require_once './vendor/twitter/twitter.class.php';
 
-$twitter = new Twitter('j2deme', 'delgadomedina');
-$status = $twitter->send('@WanaDeWich Hola');
+$consumerKey 	= "bhqtdonKSjPec3lObO7h4w";
+$consumerSecret = "bB2ohDnvAQJZExhbics52LTpe1vuf0mAeHR4dqizo";
+$accessToken	= "13940702-T0sQk1WKxeb6gJB7rqDEnnR2tdNbwgEWcey5Rwpyk";
+$accesTokenSecret = "YFac9K1zDeVMKD93VhDaRM0C6J4BrHdqAyTl8qYNis";
+
+$twitter = new Twitter($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
+$status = $twitter->send('@WanaDeWich Hola este es un tweet de prueba');
 
 echo $status ? 'OK' : 'ERROR';
 
