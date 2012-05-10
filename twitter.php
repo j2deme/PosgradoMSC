0ee599b0590e6314362ea80739e5ea9d13873ecc
Jaime Jesús Delgado Meraz <j2deme@gmail.com>
2012-05-10 16:02:09 -0500

    Changed twitter api keys & secrets

--- twitter.php
+++ twitter.php
@@ -5,8 +5,8 @@ require_once './vendor/twitter/twitter.class.php';
 
 $consumerKey 	= "EeNs24z1jl5nUW5XOlozmQ";
 $consumerSecret = "3lxhiQBzzamQzHWSKy4ESmYTZyPllbBoD2pbaEqO8";
-$accessToken	= "576620583-nj28Tp87ynNA5y7uuSNNniwF2o6oV0GELBiQ71E2";
-$accessTokenSecret = "3ZTJXe4ZRi8WuRCRb9wFGlLDwxM1CZTs0UDj8ofDu0";
+$accessToken	= "576620583-SnYj8COxgoboe3V7wxscmrdY2g60MZudyOtet7cr";
+$accessTokenSecret = "IWrm5w12XWlGMsaSTs8Hmwxn8iKAHEBKMzzwzE3im4";
 
 $twitter = new Twitter($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
 $status = $twitter->send('@wanadewich Hola este es un tweet de prueba');
