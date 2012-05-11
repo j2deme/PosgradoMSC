0a798e22f015b75e33351982d24a70197f3d97c7
Jaime Jesús Delgado Meraz <j2deme@gmail.com>
2012-05-11 15:26:36 -0500

    Added email feature

---
+++ email.php
@@ -0,0 +1,22 @@
+<?php
+require_once './vendor/swiftmailer/swift_required.php';
+
+$transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, 'ssl')
+             ->setUsername('j2deme@gmail.com')
+             ->setPassword('delgadomedina');
+
+$mailer = Swift_Mailer::newInstance($transport);
+
+$to_addresses = array('luis.hum18@gmail.com');
+$body = "<h1>Alert!</h1>";
+$body .= "<p>This is a message from the Federal Bureau of Investigation.</p><hr/><p>We are watching you.</p>";
+$message = Swift_Message::newInstance($subject)
+            ->setFrom(array('investigator@fbi.gov' => 'Captain America'))
+            ->setTo($to_addresses)
+            ->setBody($body);
+ 
+//Enviamos
+$result = $mailer->send($message);
+
+print_r($result);
+?>
--- twitter.php
+++ twitter.php
@@ -1,16 +1,28 @@
 <?php
-//ini_set ('error_reporting', E_ALL);
+
+if(isset($_POST['msg']) && isset($_POST['user'])){
+	require_once './vendor/twitter/twitter.class.php';
+
+	$consumerKey 	= "EeNs24z1jl5nUW5XOlozmQ";
+	$consumerSecret = "3lxhiQBzzamQzHWSKy4ESmYTZyPllbBoD2pbaEqO8";
+	$accessToken	= "576620583-SnYj8COxgoboe3V7wxscmrdY2g60MZudyOtet7cr";
+	$accessTokenSecret = "IWrm5w12XWlGMsaSTs8Hmwxn8iKAHEBKMzzwzE3im4";
+	
+	$twitter = new Twitter($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
+	$status = @$twitter->send('@wanadewich Hola este es un tweet de prueba');
+	
+	echo $status ? 'OK' : 'ERROR';	
+}
 
 require_once './vendor/twitter/twitter.class.php';
 
-$consumerKey 	= "EeNs24z1jl5nUW5XOlozmQ";
-$consumerSecret = "3lxhiQBzzamQzHWSKy4ESmYTZyPllbBoD2pbaEqO8";
-$accessToken	= "576620583-SnYj8COxgoboe3V7wxscmrdY2g60MZudyOtet7cr";
-$accessTokenSecret = "IWrm5w12XWlGMsaSTs8Hmwxn8iKAHEBKMzzwzE3im4";
-
-$twitter = new Twitter($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
-$status = $twitter->send('@wanadewich Hola este es un tweet de prueba');
-
-echo $status ? 'OK' : 'ERROR';
-
+	$consumerKey 	= "EeNs24z1jl5nUW5XOlozmQ";
+	$consumerSecret = "3lxhiQBzzamQzHWSKy4ESmYTZyPllbBoD2pbaEqO8";
+	$accessToken	= "576620583-SnYj8COxgoboe3V7wxscmrdY2g60MZudyOtet7cr";
+	$accessTokenSecret = "IWrm5w12XWlGMsaSTs8Hmwxn8iKAHEBKMzzwzE3im4";
+	
+	$twitter = new Twitter($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
+	$status = @$twitter->send('@wanadewich Hola este es un tweet de prueba');
+	
+	echo $status ? 'OK' : 'ERROR';
 ?>
