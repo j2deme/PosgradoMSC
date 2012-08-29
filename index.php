ef53cc46f4bca3fd966ea6a3d80bfa62e35e656e
Luis <luis.hum18@gmail.com>
2012-08-29 14:12:36 -0500

    Eliminacion de scrip extra de paco jajaja

--- index.php
+++ index.php
@@ -2054,7 +2054,7 @@ $app->get('/borrar-rol/:id/', function($id) use($app){
 //******
 $app->get('/catalogos/eventos/', function () use($app) {
 	$data['eventos'] = Evento::all();
-    $app->render('eventos.html', $data);
+    $app->render('nuevoevento.html', $data);
 })->name('CatEvento');
 
 $app->post('/nuevo-evento/', function() use($app){
